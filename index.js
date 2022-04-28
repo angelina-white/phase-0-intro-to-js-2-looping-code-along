efb8d8318f07c57a880c2000fe501e200e81d0a4
Angelina <angelinarwhite@gmail.com>
2022-04-28 11:21:02 -0400

    commit tree c5ef1b1c41b4c330b6715ab434135e97382ad39e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(names)
+{
+    let i = 0; // the initialization moves OUTSIDE the body of the loop!
+    let message = [];
+    while (i < names.length) {
+      message[i] = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      i++; // the iteration moves INSIDE the body of the loop!
+    }
+  
+    return message;
+}
+
+function countDown(num)
+{
+    let i = 0; // the initialization moves OUTSIDE the body of the loop!
+    while (i < num + 1) {
+      console.log(num);
+      num--;
+    }
+}
+
+countDown(10)
